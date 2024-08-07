3b067e55dc86f00def88184464a90801cfb6466f
Darwinst98 <djsimba@espe.edu.ec>
2024-08-06 20:58:30 -0500

    Nuevo backend

--- controller/authController.js
+++ controller/authController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const Usuario = require('../model/Usuario');
 const Ciudadano = require('../model/Ciudadano');
